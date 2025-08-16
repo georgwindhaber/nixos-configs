878919f1fe223b2db1403d4b9965d0c71286e25c
georg <georg.windhaber@gmail.com>
2025-08-16 23:02:40 +0200

    Add coding stuff to metal server

--- hosts/metal-server/configuration.nix
+++ hosts/metal-server/configuration.nix
@@ -103,7 +103,10 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+    pnpm
     git
+    vscode
+    nodejs_24
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
