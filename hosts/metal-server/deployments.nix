fb7d38a2892963fcd4c8869be36aaf9be61460ed
georg <georg.windhaber@gmail.com>
2025-11-28 15:56:27 +0100

    Add systemd services for webhooks

--- hosts/metal-server/deployments.nix
+++ hosts/metal-server/deployments.nix
@@ -8,4 +8,25 @@
       9000
     ];
   };
+
+  systemd.services.webhook-runner = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+    serviceconfig = {
+      ExecStart = "${pkgs.nodejs}/bin/node /home/georg/source/webhook-runner/dist/index.js";
+      Restart = "always";
+      WorkingDirectory = "/home/georg/source/webhook-runner";
+      Environment = "NODE_ENV=production";
+    };
+  };
+  systemd.services.repinn-backend = {
+    enable = true;
+    wantedBy = [ "multi-user.target" ];
+    serviceconfig = {
+      ExecStart = "${pkgs.nodejs}/bin/node /home/georg/source/repinn/dist/src/index.js";
+      Restart = "always";
+      WorkingDirectory = "/home/georg/source/repinn/backend";
+      Environment = "NODE_ENV=production";
+    };
+  };
 }
