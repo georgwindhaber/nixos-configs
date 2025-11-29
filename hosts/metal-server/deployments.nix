3ebd0ed67637f2cae780eec2c2e64b74b95fb990
georg <georg.windhaber@gmail.com>
2025-11-29 14:39:09 +0100

    Add dedicated service for repinn-backend

--- hosts/metal-server/deployments.nix
+++ hosts/metal-server/deployments.nix
@@ -22,4 +22,26 @@
       ExecStop = "${pkgs.pm2}/bin/pm2 kill";
     };
   };
+
+  systemd.services.repinn-backend = {
+    enable = true;
+    description = "Repinn backend";
+    unitConfig = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = "10";
+    };
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.nodejs_24}/bin/node /home/georg/projects/repinn/backend/dist/src/index.js";
+      WorkingDirectory = "/path/to/your";
+      User = "georg";
+      Group = "users";
+      # Uncomment if you need environment variables
+      # Environment = [
+      #   "NODE_ENV=production"
+      #   "PORT=3000"
+      # ];
+    };
+  };
 }
