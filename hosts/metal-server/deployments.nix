ded0dea90bde5c374b889cdeaf1c25256a39862e
georg <georg.windhaber@gmail.com>
2025-12-01 12:28:46 +0100

    Enable webhook and repinn backend service

--- hosts/metal-server/deployments.nix
+++ hosts/metal-server/deployments.nix
@@ -9,39 +9,38 @@
     ];
   };
 
-  # systemd.services.pm2 = {
-  #   enable = true;
-  #   description = "pm2";
-  #   unitConfig = {
-  #     Type = "simple";
-  #   };
-  #   wantedBy = [ "multi-user.target" ];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.pm2}/bin/pm2 resurrect";
-  #     ExecReload = "${pkgs.pm2}/bin/pm2 reload all";
-  #     ExecStop = "${pkgs.pm2}/bin/pm2 kill";
-  #   };
-  # };
+  systemd.user.services.webhook-runner = {
+    enable = true;
+    description = "Webhook runner";
+    unitConfig = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = "10";
+    };
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.nodejs_24}/bin/node /home/georg/source/webhook-runner/dist/index.js";
+      WorkingDirectory = "/home/georg/source/webhook-runner";
+    };
+  };
 
-  # systemd.services.repinn-backend = {
-  #   enable = true;
-  #   description = "Repinn backend";
-  #   unitConfig = {
-  #     Type = "simple";
-  #     Restart = "always";
-  #     RestartSec = "10";
-  #   };
-  #   wantedBy = [ "multi-user.target" ];
-  #   serviceConfig = {
-  #     ExecStart = "${pkgs.nodejs_24}/bin/node /home/georg/projects/repinn/backend/dist/src/index.js";
-  #     WorkingDirectory = "/home/georg/source/repinn/backend";
-  #     User = "georg";
-  #     Group = "users";
-  #     # Uncomment if you need environment variables
-  #     # Environment = [
-  #     #   "NODE_ENV=production"
-  #     #   "PORT=3000"
-  #     # ];
-  #   };
-  # };
+  systemd.user.services.repinn-backend = {
+    enable = true;
+    description = "Repinn backend";
+    unitConfig = {
+      Type = "simple";
+      Restart = "always";
+      RestartSec = "10";
+    };
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.nodejs_24}/bin/node /home/georg/source/repinn/backend/dist/src/index.js";
+      WorkingDirectory = "/home/georg/source/repinn/backend";
+      Environment = [
+        "PORT=3001"
+      ];
+    };
+  };
+
+
 }
