01984336eba784d280038b01f05e52bcd72f6608
georg <georg.windhaber@gmail.com>
2025-12-01 10:39:42 +0100

    Disable services

--- hosts/metal-server/deployments.nix
+++ hosts/metal-server/deployments.nix
@@ -9,39 +9,39 @@
     ];
   };
 
-  systemd.services.pm2 = {
-    enable = true;
-    description = "pm2";
-    unitConfig = {
-      Type = "simple";
-    };
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.pm2}/bin/pm2 resurrect";
-      ExecReload = "${pkgs.pm2}/bin/pm2 reload all";
-      ExecStop = "${pkgs.pm2}/bin/pm2 kill";
-    };
-  };
+  # systemd.services.pm2 = {
+  #   enable = true;
+  #   description = "pm2";
+  #   unitConfig = {
+  #     Type = "simple";
+  #   };
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.pm2}/bin/pm2 resurrect";
+  #     ExecReload = "${pkgs.pm2}/bin/pm2 reload all";
+  #     ExecStop = "${pkgs.pm2}/bin/pm2 kill";
+  #   };
+  # };
 
-  systemd.services.repinn-backend = {
-    enable = true;
-    description = "Repinn backend";
-    unitConfig = {
-      Type = "simple";
-      Restart = "always";
-      RestartSec = "10";
-    };
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.nodejs_24}/bin/node /home/georg/projects/repinn/backend/dist/src/index.js";
-      WorkingDirectory = "/home/georg/projects/repinn/backend";
-      User = "georg";
-      Group = "users";
-      # Uncomment if you need environment variables
-      # Environment = [
-      #   "NODE_ENV=production"
-      #   "PORT=3000"
-      # ];
-    };
-  };
+  # systemd.services.repinn-backend = {
+  #   enable = true;
+  #   description = "Repinn backend";
+  #   unitConfig = {
+  #     Type = "simple";
+  #     Restart = "always";
+  #     RestartSec = "10";
+  #   };
+  #   wantedBy = [ "multi-user.target" ];
+  #   serviceConfig = {
+  #     ExecStart = "${pkgs.nodejs_24}/bin/node /home/georg/projects/repinn/backend/dist/src/index.js";
+  #     WorkingDirectory = "/home/georg/source/repinn/backend";
+  #     User = "georg";
+  #     Group = "users";
+  #     # Uncomment if you need environment variables
+  #     # Environment = [
+  #     #   "NODE_ENV=production"
+  #     #   "PORT=3000"
+  #     # ];
+  #   };
+  # };
 }
