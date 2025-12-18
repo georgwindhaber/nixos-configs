9fe3a88d0c7bd00838bf48801c74508a81557e39
georg <georg.windhaber@gmail.com>
2025-12-18 10:56:13 +0100

    Remove fixed factorio version

--- hosts/metal-server/factorio/factorio.nix
+++ hosts/metal-server/factorio/factorio.nix
@@ -7,17 +7,6 @@
   services.factorio = {
     enable = true;
     openFirewall = true;
-    package = pkgs.factorio-headless.override {
-      versionsJson =
-        let
-          nixpkgsCommit = "master";
-          versions = pkgs.fetchurl {
-            url = "https://raw.githubusercontent.com/NixOS/nixpkgs/refs/heads/${nixpkgsCommit}/pkgs/by-name/fa/factorio/versions.json";
-            sha256 = "sha256-6EBb4JfcCQ/iE+PVRKxAbPkqMDvBM12rK8sHgg6zSQ4=";
-          };
-        in
-        "${versions}";
-    };
     loadLatestSave = true;
     lan = true;
     saveName = "testserver";
