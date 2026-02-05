ed1952defa17669573f80fe8917306b9957c7cdc
georg <georg.windhaber@gmail.com>
2026-02-05 22:49:42 +0100

    Up maennerhoele wiki port

--- hosts/metal-server/wiki.nix
+++ hosts/metal-server/wiki.nix
@@ -1,25 +1,25 @@
 { config, pkgs, ... }:
 
 {
-	networking.firewall = {
+  networking.firewall = {
     allowedTCPPorts = [
-      3003
+      3004
     ];
-	};
-	systemd.services.wiki-js = {
-		requires = [ "postgresql.service" ];
-		after    = [ "postgresql.service" ];
-	};
-	services.wiki-js = {
-		enable = true;
-		settings = {
-			port = 3003;
-			db = {
-				db  = "wiki-js";
-				host = "/run/postgresql";
-				type = "postgres";
-				user = "wiki-js";
-			};
-		};
-	};
+  };
+  systemd.services.wiki-js = {
+    requires = [ "postgresql.service" ];
+    after = [ "postgresql.service" ];
+  };
+  services.wiki-js = {
+    enable = true;
+    settings = {
+      port = 3004;
+      db = {
+        db = "wiki-js";
+        host = "/run/postgresql";
+        type = "postgres";
+        user = "wiki-js";
+      };
+    };
+  };
 }
